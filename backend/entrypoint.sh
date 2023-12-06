6fb5adb5b3831d71940393bd963b9e85834de75e
Illés Tamás <illes.tamas@grape.solutions>
2023-12-06 17:10:31 +0100

    docker-compose and entrypoint for install cert

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
 echo $TZ > /etc/timezone
+
+if [ "$INSTALL_FW_CERTIFICATE" = "onprem" ];
+then
+   echo " ===> INSTALLING CERTIFICATE"
+   update-ca-certificates
+else
+   echo " ===> NO EXTERNAL CERTIFICATE INSTALLED"
+fi
+
 exec "$@"
