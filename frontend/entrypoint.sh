00f19eaa783b1a1cc90fa1703475d84eac9c8053
Kindla János <Kindla.Janos@grape.solutions>
2025-10-15 11:40:53 +0000

    feat: add new env var

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 find /app -type f | xargs sed -i \
- -e 's,%REACT_APP_API_URL%,'"$REACT_APP_API_URL"',g'
+ -e 's,%REACT_APP_API_URL%,'"$REACT_APP_API_URL"',g' \
+ -e 's,%REACT_APP_ODIN_URL%,'"$REACT_APP_ODIN_URL"',g'
 
 exec "$@"
